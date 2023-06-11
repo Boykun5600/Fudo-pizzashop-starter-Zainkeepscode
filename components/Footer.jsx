21bbcc33989327a7a35acc570427755588faeba1
Boykun5600 <boyandboa@gmail.com>
2023-06-11 17:41:33 +0700

    commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,22 @@
-import {UilFacebook, UilGithup, UilInstageam} from "@iconscout/react-unicons"
-import css from '../styles/Footer.module.css'
+import {UilFacebook, UilGithup, UilInstagram} from "@iconscout/react-unicons";
+import css from '../styles/Footer.module.css';
+import Image from "next/image";
+import Logo from "../styles/Logo.png";
 export default function Footer() {
     return(
         <div className={css.container}>
             <span>ALL RIGHT RESERVED</span>
-            <div className={css.social}>
-                <UilFacebook/>
-                <UilGithup/>
-                <UilInstageam/>
+             <div className={css.social}>
+                <UilFacebook size={45}/>
+                <UilGithup size={45}/>
+                <UilInstagram size={45}/>
+             </div>
+
+             <div className={css.logo}>
+                <Image src ={Logo} alt = "" width={50} height={50} />
+                <span>Fudo</span>
             </div>
+
          </div>
     )
     
