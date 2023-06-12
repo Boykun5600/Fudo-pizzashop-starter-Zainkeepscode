4aa712e480c90066dab60a142a9ffa2778673ba4
Boykun5600 <boyandboa@gmail.com>
2023-06-12 12:57:00 +0700

    commit

---
+++ components/Hero.jsx
@@ -0,0 +1,9 @@
+import css from '../styles/Hero.module.css'
+export default function Hero() {
+    return(
+        <div className={CSS.container}>
+            hero
+        </div>
+    )
+    
+};
