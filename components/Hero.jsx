1f9afa7f33fa78271f7c729e4677c79a9fbf447e
Boykun5600 <boyandboa@gmail.com>
2023-06-12 13:05:48 +0700

    commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import css from '../styles/Hero.module.css'
+import Cherry from '../assets/Cherry.png'
 export default function Hero() {
     return(
         <div className={CSS.container}>
-            hero
+            {/* left side */}
+
+            <div className={css.cherryDiv}>
+                <span>More than Faster</span>
+                <Image src={Cherry} alt=""/>
+            </div>
+            {/* right side */}
         </div>
     )
     
