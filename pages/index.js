222bfe61439842d76b2f508c4a7838e74faa5d6b
Boykun5600 <boyandboa@gmail.com>
2023-06-12 12:57:40 +0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/Layout";
-
+import Hero from "../components/Hero";
 export default function Home() {
   return (
     <Layout>
@@ -13,10 +13,9 @@ export default function Home() {
         </Head>
         {/* body */}
         <main>
-          <h1>Subscribe Zainkeepscode boy </h1>        
+         <Hero/>      
         </main>
       </div>
-    
     </Layout>
   );
 }
